e4905edbafcc1bbe448e30f19fa32817d6900f3c
Nicholas Salyzyn <nsalyzyn@gmail.com>
2011-02-26 14:20:36 -0800

    Initial setting up with relation to canvas draw module.

---
+++ src/main/java/com/nsalz/gwt/canvas/create/client/TestComposite.java
@@ -0,0 +1,11 @@
+package com.nsalz.gwt.canvas.create.client;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+
+public class TestComposite extends Composite
+{
+    public TestComposite() {
+        initWidget(new Label("Change!"));
+    }
+}
